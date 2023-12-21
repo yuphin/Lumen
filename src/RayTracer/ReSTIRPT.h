3ef67bed159019e78489a895de0bf2fe85c3b385
yuphin <yunus.c.cakir@gmail.com>
2023-12-21 01:51:27 +0100

    Enable by default

--- src/RayTracer/ReSTIRPT.h
+++ src/RayTracer/ReSTIRPT.h
@@ -39,7 +39,7 @@ class ReSTIRPT : public Integrator {
 	bool canonical_only = false;
 	bool show_reconnection_radiance = false;
 	bool enable_temporal_reuse = true;
-	bool enable_gris = false;
+	bool enable_gris = true;
 	bool pixel_debug = false;
 	bool enable_permutation_sampling = false;
 	bool enable_atmosphere = false;
